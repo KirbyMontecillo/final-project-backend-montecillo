efd6dbf2cf3be2133d6f302cb6a1cbd1419855b9
Kirby Nicole Montecillo <kirbynicolemontecillo@gmail.com>
2026-05-18 02:15:21 +0800

    tried gmail smtp

--- _helpers/send-email.ts
+++ _helpers/send-email.ts
@@ -1,10 +1,11 @@
 import nodemailer from 'nodemailer';
 
 export default async function sendEmail({ to, subject, html, from = process.env.SMTP_USER }: any) {
+    const port = Number(process.env.SMTP_PORT) || 587;
     const transporter = nodemailer.createTransport({
         host: process.env.SMTP_HOST,
-        port: Number(process.env.SMTP_PORT) || 587,
-        secure: false,
+        port: port,
+        secure: port === 465,
         auth: {
             user: process.env.SMTP_USER,
             pass: process.env.SMTP_PASS
